f39e8e38ee8c7b46132f43e35f783cda1529a4fa
Johar Putra Artemi johar <johar.putra@DDC.LOCAL>
2020-08-27 17:10:37 +0700

    remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -50,41 +50,7 @@ class _MyAppState extends State<MyApp> {
     ));
   }
 
-  List<Image> splitImage(
-      {imglib.Image inputImage,
-      int horizontalPieceCount,
-      int verticalPieceCount}) {
-    imglib.Image image = inputImage as imglib.Image;
 
-    final int xLength = (image.width / horizontalPieceCount).round();
-    final int yLength = (image.height / verticalPieceCount).round();
-    List<imglib.Image> pieceList = List<imglib.Image>();
 
-    for (int y = 0; y < verticalPieceCount; y++)
-      for (int x = 0; x < horizontalPieceCount; x++) {
-        pieceList.add(
-          imglib.copyCrop(image, x, y, x * xLength, y * yLength),
-        );
-      }
 
-    //Convert image from image package to Image Widget to display
-    List<Image> outputImageList = List<Image>();
-    for (imglib.Image img in pieceList) {
-      outputImageList.add(Image.memory(imglib.encodeJpg(img)));
-    }
-
-    return outputImageList;
-  }
-
-  imglib.Image copyCrop(imglib.Image src, int x, int y, int w, int h) {
-    imglib.Image dst = imglib.Image(w, h,
-        channels: src.channels, exif: src.exif, iccp: src.iccProfile);
-
-    for (int yi = 0, sy = y; yi < h; ++yi, ++sy) {
-      for (int xi = 0, sx = x; xi < w; ++xi, ++sx) {
-        dst.setPixel(xi, yi, src.getPixel(sx, sy));
-      }
-    }
-    return dst;
-  }
 }
